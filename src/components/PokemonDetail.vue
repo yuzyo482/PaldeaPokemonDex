bed011f292be3f117738ce8a99fd6d8aac077257
yuz <bartilous@email.com>
2024-04-24 17:33:14 +0800

    statsfix

--- src/components/PokemonDetail.vue
+++ src/components/PokemonDetail.vue
@@ -202,7 +202,11 @@ export default {
       }
 
       .stats {
+        width: 60%;
+
+        @media screen and (max-width: 1440px) {
         width: 70%;
+      }
 
         @media screen and (max-width: 1080px) {
         width: 90%;
